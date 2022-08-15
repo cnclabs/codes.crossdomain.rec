6343941a84f2e66f2f81e4457da948ea6f1acf1d
hi-i-m-gtooth <noface21105@gmail.com>
2022-08-15 16:33:11 +0800

    LGN dependency update -- yzliu@0815

--- baseline/LGN/preprocess/run_preprocess.sh
+++ baseline/LGN/preprocess/run_preprocess.sh
@@ -1,6 +1,6 @@
-datasets=(hk_csjj)
+declare -a datasets=("hk_csjj" "spo_csj" "mt_b")
 declare -A ncores
-ncores=(['hk_csjj']=5)
+ncores=(['hk_csjj']=5 ["spo_csj"]=5 ["mt_b"]=10)
 
 for d in "${datasets[@]}"; do
 	python3 generate_ncore_input.py --dataset ${d} --ncore ${ncores[$d]}
