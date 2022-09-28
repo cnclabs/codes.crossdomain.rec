cf8d37d0276d789014377cea64db322881a55e31
yth <yuting2727@gamil.com>
2022-09-28 04:17:24 +0000

    chore: load/save data from assigned dir

--- baseline/LGN/preprocess/run_preprocess.sh
+++ baseline/LGN/preprocess/run_preprocess.sh
@@ -1,7 +1,8 @@
 declare -a datasets=("hk_csjj" "spo_csj" "mt_b")
 declare -A ncores
+mom_save_dir='/TOP/tmp2/cpr/from_yzliu/'
 ncores=(['hk_csjj']=5 ["spo_csj"]=5 ["mt_b"]=10)
 
 for d in "${datasets[@]}"; do
-	python3 generate_ncore_input.py --dataset ${d} --ncore ${ncores[$d]}
+	python3 generate_ncore_input.py --dataset ${d} --ncore ${ncores[$d]} --mom_save_dir ${mom_save_dir}
 done
