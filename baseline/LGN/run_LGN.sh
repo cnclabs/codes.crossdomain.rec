653b5508a0e18e18f7851d6a05534f656f618b5f
yth <yuting2727@gmail.com>
2022-10-01 07:06:25 +0000

    modify output function -- yzliu@0926

--- baseline/LGN/run_LGN.sh
+++ baseline/LGN/run_LGN.sh
@@ -37,7 +37,7 @@ do
 				fullname=${dataset}_${graph}_${te}
 				python3 edit_properties.py --dataset ${fullname}
 				screen -dm -S ${fullname} bash -c \
-				"python3 main.py; python3 ../../rec_and_eval_ncore.py \
+				"python3 ../../rec_and_eval_ncore.py \
 				--test_users ${te} \
 				--output_file $(pwd)/result/${src}_${tar}_lightgcn_result_${epoch}_${te}.txt \
 				--graph_file $(pwd)/graph/${fullname}_${epoch}epoch.graph \
