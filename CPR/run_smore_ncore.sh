1fc3a6bd3e3d3cc24d1c930e15d9c1da5542b056
yth <yuting2727@gmail.com>
2022-10-12 12:01:57 +0000

    chore:
    explicit argument name,
    save result to assign dir,
    n testing sample:  3500 -> 4000

--- CPR/run_smore_ncore.sh
+++ CPR/run_smore_ncore.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-mom_save_dir=$1
-sample=4000
+data_dir=$1
+model_save_dir=$2
+sample=3500
 declare -a datasets=("hk_csjj" "spo_csj" "mt_b")
 declare -A ncores
-ncores=(['hk_csjj']=5 ["spo_csj"]=5 ["mt_b"]=10)
+ncores=(['hk_csjj']=5 ["spo_csj"]=5 ["mt_b"]=5)
 
-if [[ ! -d ./result ]]
+if [[ ! -d ${model_save_dir}/graph ]]
 then
-	mkdir -p ./graph
-	mkdir -p ./result
+	mkdir -p ${model_save_dir}/graph
+	mkdir -p ${model_save_dir}/result
 fi
 
 for d in "${datasets[@]}"; do
@@ -21,55 +22,55 @@ for d in "${datasets[@]}"; do
 
 	for i in $(seq 2 2);
 	do
-	epoch=$((i*100))
+	epoch=$((i*1))
 
 	## ================================= Amazon dataset (SRC-TAR) =================================
 	if [ $i == 1 ]; then
 		pretrain=""
 	else
-		pretrain="-pre-train ./graph/all_${src}_${tar}_cpr_ug_0.01_ig_0.06_$((epoch-100))epoch.txt"
-		coldpretrain="-pre-train ./graph/cold_all_${src}_${tar}_cpr_ug_0.01_ig_0.06_$((epoch-100))epoch.txt"
+		pretrain="-pre-train ${model_save_dir}/graph/all_${src}_${tar}_cpr_ug_0.01_ig_0.06_$((epoch-100))epoch.txt"
+		coldpretrain="-pre-train ${model_save_dir}/graph/cold_all_${src}_${tar}_cpr_ug_0.01_ig_0.06_$((epoch-100))epoch.txt"
 	fi
-	./ADS_crossDomainRec/smore-stack/pre-train_changeUpt_cpr \
-	-train_ut ${mom_save_dir}/input_${ncore}core/${tar}_train_input.txt \
-	-train_us ${mom_save_dir}/input_${ncore}core/all_${src}_train_input.txt \
-	-train_ust ${mom_save_dir}/input_${ncore}core/all_cpr_train_u_${src}+${tar}.txt \
-	-save ./graph/all_${src}_${tar}_cpr_ug_0.01_ig_0.06_$((epoch))epoch.txt \
-	-dimension 100 -update_times $((epoch)) -worker 16 -init_alpha 0.025 -user_reg 0.01 -item_reg 0.06 
-	#$pretrain
+	#./ADS_crossDomainRec/smore-stack/pre-train_changeUpt_cpr \
+	#-train_ut ${data_dir}/input_${ncore}core/${tar}_train_input.txt \
+	#-train_us ${data_dir}/input_${ncore}core/all_${src}_train_input.txt \
+	#-train_ust ${data_dir}/input_${ncore}core/all_cpr_train_u_${src}+${tar}.txt \
+	#-save ${model_save_dir}/graph/all_${src}_${tar}_cpr_ug_0.01_ig_0.06_$((epoch))epoch.txt \
+	#-dimension 100 -update_times $((epoch)) -worker 16 -init_alpha 0.025 -user_reg 0.01 -item_reg 0.06 
+	##$pretrain
 
 	python3 ../rec_and_eval_ncore.py \
-	--mom_save_dir ${mom_save_dir} \
+	--data_dir ${data_dir} \
 	--test_users target \
-	--output_file $(pwd)/result/all_${src}_${tar}_cpr_target_result_$((epoch))epoch.txt \
-	--graph_file $(pwd)/graph/all_${src}_${tar}_cpr_ug_0.01_ig_0.06_$((epoch))epoch.txt \
+	--output_file ${model_save_dir}/result/all_${src}_${tar}_cpr_target_result_$((epoch))epoch.txt \
+	--graph_file ${model_save_dir}/graph/all_${src}_${tar}_cpr_ug_0.01_ig_0.06_$((epoch))epoch.txt \
 	--src ${src} \
 	--tar ${tar} \
 	--ncore ${ncore}
 
 	python3 ../rec_and_eval_ncore.py \
-	--mom_save_dir ${mom_save_dir} \
+	--data_dir ${data_dir} \
 	--test_users shared \
-	--output_file $(pwd)/result/all_${src}_${tar}_cpr_shared_result_$((epoch))epoch.txt \
-	--graph_file $(pwd)/graph/all_${src}_${tar}_cpr_ug_0.01_ig_0.06_$((epoch))epoch.txt \
+	--output_file ${model_save_dir}/result/all_${src}_${tar}_cpr_shared_result_$((epoch))epoch.txt \
+	--graph_file ${model_save_dir}/graph/all_${src}_${tar}_cpr_ug_0.01_ig_0.06_$((epoch))epoch.txt \
 	--src ${src} \
 	--tar ${tar} \
 	--ncore ${ncore}
 
 	# cold
 	./ADS_crossDomainRec/smore-stack/pre-train_changeUpt_cpr \
-	-train_ut ${mom_save_dir}/input_${ncore}core/cold_${tar}_train_input.txt \
-	-train_us ${mom_save_dir}/input_${ncore}core/all_${src}_train_input.txt \
-	-train_ust ${mom_save_dir}/input_${ncore}core/cold_cpr_train_u_${src}+${tar}.txt \
-	-save ./graph/cold_all_${src}_${tar}_cpr_ug_0.01_ig_0.06_$((epoch))epoch.txt \
+	-train_ut ${data_dir}/input_${ncore}core/cold_${tar}_train_input.txt \
+	-train_us ${data_dir}/input_${ncore}core/all_${src}_train_input.txt \
+	-train_ust ${data_dir}/input_${ncore}core/cold_cpr_train_u_${src}+${tar}.txt \
+	-save ${model_save_dir}/graph/cold_all_${src}_${tar}_cpr_ug_0.01_ig_0.06_$((epoch))epoch.txt \
 	-dimension 100 -update_times $((epoch)) -worker 16 -init_alpha 0.025 -user_reg 0.01 -item_reg 0.06 
 	#$coldpretrain
 
 	python3 ../rec_and_eval_ncore.py \
-	--mom_save_dir ${mom_save_dir} \
+	--data_dir ${data_dir} \
 	--test_users cold \
-	--output_file $(pwd)/result/all_${src}_${tar}_cpr_cold_result_$((epoch))epoch.txt \
-	--graph_file $(pwd)/graph/cold_all_${src}_${tar}_cpr_ug_0.01_ig_0.06_$((epoch))epoch.txt \
+	--output_file ${model_save_dir}/result/all_${src}_${tar}_cpr_cold_result_$((epoch))epoch.txt \
+	--graph_file ${model_save_dir}/graph/cold_all_${src}_${tar}_cpr_ug_0.01_ig_0.06_$((epoch))epoch.txt \
 	--src ${src} \
 	--tar ${tar} \
 	--ncore ${ncore}
