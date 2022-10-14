54e7dd1b1e649bc1a669c961ed4579c17716a3d3
yth <yuting2727@gmail.com>
2022-10-14 15:55:25 +0000

    tmp: merge to 1

--- rec_and_eval_ncore.py
+++ rec_and_eval_ncore.py
@@ -15,6 +15,35 @@ import uuid
 
 from evaluation.tools import save_exp_record
 
+def generate_item_graph_df(graph_file):
+    st = time.time()
+    def _text_to_array(cell):
+        emb = np.array(cell.split(), dtype=np.float32)
+        emb = np.expand_dims(emb, axis=-1)
+        
+        return emb
+        
+    graph_df = pd.read_csv(graph_file, sep='\t', header=None, names=['node_id', 'embed'])
+    item_graph_df = graph_df[~graph_df['node_id'].str.startswith('user_')]
+    item_graph_df['embed'] = item_graph_df['embed'].apply(_text_to_array)
+
+    print('item graph shape:', item_graph_df.shape)
+    print('Finished gen item graph df!', time.time() - st)
+    
+    return item_graph_df
+
+def generate_user_emb(graph_file):
+    user_emb={}
+    with open(graph_file, 'r') as f:
+        for line in f:
+            line = line.split('\t')
+            prefix = line[0]
+            emb=line[1].split()
+            #if prefix in testing_users:
+            if "user_" in prefix:
+                user_emb.update({ prefix: np.array(emb, dtype=np.float32) })
+    return user_emb
+
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 parser=argparse.ArgumentParser(description='Calculate the similarity and recommend VOD items')
@@ -71,10 +100,6 @@ tar_train_df[args.uid_u] = tar_train_df[args.uid_u].apply(lambda x: 'user_'+x)
 tar_test_df[args.uid_u]  = tar_test_df[args.uid_u].apply(lambda x: 'user_'+x)
 total_item_set = set(tar_train_df[args.uid_i])
 
-
-# ground truth, csjj is target
-# Generate user 100 rec pool
-
 def process_user_pos_neg_pair(user_list):
   user_rec_dict = {}
 
@@ -105,69 +130,40 @@ for r in results:
 
 print("Done generating testing users' positive-negative pairs.")
 
-user_emb={}
-item_emb={}
-
-# get emb of testing users and all (training) items
 print("Start getting embedding for each user and item...")
-with open(graph_file, 'r') as f:
-    for line in f:
-        line = line.split('\t')
-        prefix = line[0]
-        emb=line[1].split()
-        #if prefix in testing_users:
-        if "user_" in prefix:
-            user_emb.update({ prefix: np.array(emb, dtype=np.float32) })
-        else:
-            item_emb.update({ prefix: np.array(emb, dtype=np.float32) })
-
+user_emb = generate_user_emb(graph_file)
+item_graph_df= generate_item_graph_df(graph_file)
 print("Got embedding!")
 
-k_amount = args.top_ks
-k_max = max(k_amount)
-
-d=100
-count = 0
-total_rec=[0, 0, 0, 0, 0]
-total_ndcg=[0, 0, 0, 0, 0]
 
 print("Testing users amount = {}".format(len(testing_users)))
 print("Start counting...")
+k_amount = args.top_ks
+k_max = max(k_amount)
+def get_top_rec_and_eval(user):
+    total_rec=[0, 0, 0, 0, 0]
+    total_ndcg=[0, 0, 0, 0, 0]
+    d=100
+    if user not in user_emb.keys():
+      return total_rec, total_ndcg, 0
 
-st = time.time()
-def text_to_array(cell):
-    emb = np.array(cell.split(), dtype=np.float32)
-    emb = np.expand_dims(emb, axis=-1)
-    
-    return emb
-    
-graph_df = pd.read_csv(graph_file, sep='\t', header=None, names=['node_id', 'embed'])
-item_graph_df = graph_df[~graph_df['node_id'].str.startswith('user_')]
-item_graph_df['embed'] = item_graph_df['embed'].apply(text_to_array)
-
-print('item graph shape:', item_graph_df.shape)
-print('Finished gen item graph df!', time.time() - st)
-
-st = time.time()
-for user in testing_users:
-    count+=1
     user_emb_vec = np.array(list(user_emb[user]))
     user_emb_vec_m = np.matrix(user_emb_vec)
+    
     user_rec_pool = testing_users_rec_dict[user]
-
+    
     _tmp_df = item_graph_df[item_graph_df['node_id'].isin(user_rec_pool)]
     item_emb_vec = np.concatenate(list(_tmp_df['embed']), axis = -1).T
     item_emb_vec = item_emb_vec.copy(order='C')
     
     item_key = np.array(list(_tmp_df['node_id']))
 
+    if len(user_rec_pool) == 0:
+      return total_rec, total_ndcg, 0
     index = faiss.IndexFlatIP(d)
     index.add(item_emb_vec)
     D, I = index.search(user_emb_vec_m, k_max)
     recomm_list = item_key[I][0]
-
-    if count%1000 == 0:
-        print("{} users counted.".format(count))
 
     # ground truth
     test_data = list(tar_test_df[tar_test_df[args.uid_u] == user][args.uid_i])
@@ -177,13 +173,25 @@ for user in testing_users:
         total_rec[k]+=calculate_Recall(test_data, recomm_k)
         total_ndcg[k]+=calculate_NDCG(test_data, recomm_k)
 
-total_rec=np.array(total_rec)
-total_ndcg=np.array(total_ndcg)
+    return total_rec, total_ndcg, 1
+
+st = time.time()
+with Pool(processes=args.n_worker) as pool:
+    m = pool.map(get_top_rec_and_eval, testing_users)
+
 print('Done counting.', time.time() - st)
+
+total_rec = []
+total_ndcg = []
+for rec, ndcg, _ in m:
+  total_rec.append(rec)
+  total_ndcg.append(ndcg)
+count = len(total_rec) 
+total_rec = np.sum(np.array(total_rec), axis=0)
+total_ndcg = np.sum(np.array(total_ndcg), axis=0)
 
 model_name = args.model_name
 dataset_pair = f"{src}_{tar}"
 test_mode=args.test_mode
 top_ks = k_amount
-
 save_exp_record(model_name, dataset_pair, test_mode, top_ks, total_rec, total_ndcg, count, save_dir, save_name, output_file)
