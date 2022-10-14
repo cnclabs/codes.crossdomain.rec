4d17e29771ce068676f18fc75fba08633f5432e2
yth <yuting2727@gmail.com>
2022-10-14 04:15:07 +0000

    chore: related changes bitgcf

--- baseline/Bi-TGCF/single_bitgcf.sh
+++ baseline/Bi-TGCF/single_bitgcf.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-mom_save_dir=/TOP/tmp2/cpr/from_yzliu
-sample=4000
-
-declare -A ncores
-ncores=(['hk_csjj']=5 ["spo_csj"]=5 ["mt_b"]=10)
-
 src=$1
 tar=$2
 gpu_id=$3
 mode=$4
+data_dir=$5
+exp_record_dir=$6
+n_epoch=200 # current code will save only {20, 40,....}
+model_name=bitgcf
+sample=3500
+
+declare -A ncores
+ncores=(['hk_csjj']=5 ["spo_csj"]=5 ["mt_b"]=5)
 ncore=${ncores[${src}_${tar}]}
 
 if [[ ! -d ./graph ]]
@@ -24,50 +26,56 @@ fi
 if [[ $mode == "normal" ]]
 then	
 	CUDA_VISIBLE_DEVICES=${gpu_id} \
-        python ./BiTGCF/main.py \
-            --data_path ${mom_save_dir}/input_${ncore}core \
+        python3 ./BiTGCF/main.py \
+            --data_path ${data_dir}/input_${ncore}core \
             --source_dataset all_${src} \
             --target_dataset ${tar} \
-            --epoch 200 \
+            --epoch ${n_epoch} \
             --batch_size 65536 \
             --embed_size 25&&
         python3 ../../rec_and_eval_ncore.py \
-            --mom_save_dir ${mom_save_dir} \
-            --test_users target \
+	    --data_dir ${data_dir} \
+	    --test_mode target \
+            --save_dir ${exp_record_dir} \
+	    --save_name M_${model_name}_D_${src}_${tar}_T_target \
             --output_file $(pwd)/result/${src}_${tar}_bitgcf_result_${update_times}_target.txt \
-            --graph_file $(pwd)/graph/all_${src}_${tar}_200.graph \
+            --graph_file $(pwd)/graph/all_${src}_${tar}_${n_epoch}.graph \
             --src ${src} \
             --tar ${tar} \
-            --ncore ${ncore} \
-            --sample ${sample}&&
+            --model_name ${model_name} \
+            --ncore ${ncore}&&
         python3 ../../rec_and_eval_ncore.py \
-            --mom_save_dir ${mom_save_dir} \
-            --test_users shared \
+	    --data_dir ${data_dir} \
+	    --test_mode shared \
+            --save_dir ${exp_record_dir} \
+	    --save_name M_${model_name}_D_${src}_${tar}_T_shared \
             --output_file $(pwd)/result/${src}_${tar}_bitgcf_result_${update_times}_shared.txt \
-            --graph_file $(pwd)/graph/all_${src}_${tar}_200.graph \
+            --graph_file $(pwd)/graph/all_${src}_${tar}_${n_epoch}.graph \
             --src ${src} \
             --tar ${tar} \
-            --ncore ${ncore} \
-            --sample ${sample}
+            --model_name ${model_name} \
+            --ncore ${ncore}
 fi 
 
 if [[ $mode == "cold" ]]
 then
 	CUDA_VISIBLE_DEVICES=${gpu_id} \
         python3 ./BiTGCF/main.py\
-            --data_path ${mom_save_dir}/input_${ncore}core \
+            --data_path ${data_dir}/input_${ncore}core \
             --source_dataset all_${src} \
             --target_dataset cold_${tar} \
-            --epoch 200 \
+            --epoch ${n_epoch} \
             --batch_size 65536 \
             --embed_size 25&&
         python3 ../../rec_and_eval_ncore.py \
-            --mom_save_dir ${mom_save_dir} \
-            --test_users cold \
+	    --data_dir ${data_dir} \
+	    --test_mode cold \
+            --save_dir ${exp_record_dir} \
+	    --save_name M_${model_name}_D_${src}_${tar}_T_cold \
             --output_file $(pwd)/result/${src}_${tar}_bitgcf_result_${update_times}_cold.txt \
-            --graph_file $(pwd)/graph/all_${src}_cold_${tar}_200.graph \
+            --graph_file $(pwd)/graph/all_${src}_cold_${tar}_${n_epoch}.graph \
             --src ${src} \
             --tar ${tar} \
-            --ncore ${ncore} \
-            --sample ${sample}
+            --model_name ${model_name}\
+            --ncore ${ncore}
 fi
