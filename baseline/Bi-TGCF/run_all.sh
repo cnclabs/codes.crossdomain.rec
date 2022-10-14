beea165164500e43c6cd5ce7b54805238d4a7f0d
yth <yuting2727@gmail.com>
2022-10-14 04:16:04 +0000

    chore: related changes bitgcf

--- baseline/Bi-TGCF/run_all.sh
+++ baseline/Bi-TGCF/run_all.sh
@@ -1,6 +1,8 @@
-bash single_bitgcf.sh hk csjj 0 normal &
-bash single_bitgcf.sh hk csjj 0 cold &
-bash single_bitgcf.sh spo csj 1 normal &
-bash single_bitgcf.sh spo csj 1 cold &
-bash single_bitgcf.sh mt b 2 normal & 
-bash single_bitgcf.sh mt b 2 cold
+data_dir=$1
+exp_record_dir=$2
+bash single_bitgcf.sh hk csjj 0 normal ${data_dir} ${exp_record_dir}&
+bash single_bitgcf.sh hk csjj 0 cold ${data_dir} ${exp_record_dir}&
+bash single_bitgcf.sh spo csj 1 normal ${data_dir} ${exp_record_dir}&
+bash single_bitgcf.sh spo csj 1 cold ${data_dir} ${exp_record_dir}&
+bash single_bitgcf.sh mt b 2 normal ${data_dir} ${exp_record_dir}& 
+bash single_bitgcf.sh mt b 2 cold ${data_dir} ${exp_record_dir}
