d929a0e603ee67227a4f303d09831b7975935923
yth <yuting2727@gmail.com>
2022-10-15 08:09:46 +0000

    remove useless txt file writing

--- evaluation/utility.py
+++ evaluation/utility.py
@@ -77,7 +77,7 @@ def generate_user_emb(graph_file):
 def save_exp_record(model_name, dataset_pair, test_mode, top_ks, total_rec, total_ndcg, count, save_dir, save_name, output_file):
     uuid_str = uuid.uuid4().hex
     record_row_save_path = os.path.join(save_dir, save_name +'_' +uuid_str+'.csv')
-    txt_contents = []
+    #txt_contents = []
     record_row = {}
     record_row['model_name'] = model_name
     record_row['dataset_pair'] = dataset_pair
@@ -87,26 +87,26 @@ def save_exp_record(model_name, dataset_pair, test_mode, top_ks, total_rec, tota
     for idx, k in enumerate(top_ks):
         _recall = total_rec[idx]/count
         _ndcg   = total_ndcg[idx]/count
-        _content = [
-               '\n--------------------------------',
-               f'\n recall@{k}: ',
-                str(_recall),
-               f'\n NDCG@{k}: ',
-                str(_ndcg)]
-        txt_contents.append(_content)
+        #_content = [
+        #       '\n--------------------------------',
+        #       f'\n recall@{k}: ',
+        #        str(_recall),
+        #       f'\n NDCG@{k}: ',
+        #        str(_ndcg)]
+        #txt_contents.append(_content)
         record_row[f'recall@{k}'] = _recall
         record_row[f'NDCG@{k}'] = _ndcg
     
     record_row = pd.DataFrame([record_row]) 
     record_row.to_csv(record_row_save_path, index=False)
     
-    print("Start writing file...")
-    with open(output_file, 'w') as fw:
-        fw.writelines(['=================================\n',
-                '\n evaluated users: ',
-                str(count)])
-        for _content in txt_contents:
-            fw.writelines(_content)
+    #print("Start writing file...")
+    #with open(output_file, 'w') as fw:
+    #    fw.writelines(['=================================\n',
+    #            '\n evaluated users: ',
+    #            str(count)])
+    #    for _content in txt_contents:
+    #        fw.writelines(_content)
     print('Finished!')
 
 def rank_and_score(testing_users, top_ks, user_emb, testing_users_rec_dict, item_graph_df, tar_test_df, n_worker, uid_u, uid_i):
