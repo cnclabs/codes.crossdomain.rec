379b846dea84a6217cf69688d209fb506a360cb8
yth <yuting2727@gmail.com>
2022-10-18 02:21:17 +0000

    chore:

--- baseline/Bi-TGCF/run_all.sh
+++ baseline/Bi-TGCF/run_all.sh
@@ -1,8 +1,9 @@
 data_dir=$1
 exp_record_dir=$2
-bash single_bitgcf.sh hk csjj 0 normal ${data_dir} ${exp_record_dir}&
-bash single_bitgcf.sh hk csjj 0 cold ${data_dir} ${exp_record_dir}&
-bash single_bitgcf.sh spo csj 1 normal ${data_dir} ${exp_record_dir}&
-bash single_bitgcf.sh spo csj 1 cold ${data_dir} ${exp_record_dir}&
-bash single_bitgcf.sh mt b 2 normal ${data_dir} ${exp_record_dir}& 
-bash single_bitgcf.sh mt b 2 cold ${data_dir} ${exp_record_dir}
+mode=$3
+bash single_bitgcf.sh hk csjj 0 normal ${data_dir} ${exp_record_dir} ${mode}&
+bash single_bitgcf.sh hk csjj 0 cold ${data_dir} ${exp_record_dir} ${mode}&
+bash single_bitgcf.sh spo csj 1 normal ${data_dir} ${exp_record_dir} ${mode}&
+bash single_bitgcf.sh spo csj 1 cold ${data_dir} ${exp_record_dir} ${mode}&
+bash single_bitgcf.sh mt b 2 normal ${data_dir} ${exp_record_dir} ${mode}& 
+bash single_bitgcf.sh mt b 2 cold ${data_dir} ${exp_record_dir} ${mode}
--- baseline/Bi-TGCF/single_bitgcf.sh
+++ baseline/Bi-TGCF/single_bitgcf.sh
@@ -2,9 +2,10 @@
 src=$1
 tar=$2
 gpu_id=$3
-mode=$4
+normal_cold=$4
 data_dir=$5
 exp_record_dir=$6
+mode=$7
 n_epoch=200 # current code will save only {20, 40,....}
 model_name=bitgcf
 sample=3500
@@ -23,8 +24,10 @@ then
 	mkdir -p ./result
 fi
 
-if [[ $mode == "normal" ]]
-then	
+if [[ $normal_cod == "normal" ]]
+then
+    if [[ "$mode" == "train" || "$mode" == "traineval" ]]
+    then 
 	CUDA_VISIBLE_DEVICES=${gpu_id} \
         python3 ./BiTGCF/main.py \
             --data_path ${data_dir}/input_${ncore}core \
@@ -32,14 +35,18 @@ then
             --target_dataset ${tar} \
             --epoch ${n_epoch} \
             --batch_size 65536 \
-            --embed_size 25&&
+            --embed_size 25
+    fi
+
+    if [[ "$mode" == "eval" || "$mode" == "traineval" ]]
+    then
         python3 ../../rec_and_eval_ncore.py \
 	    --data_dir ${data_dir} \
 	    --test_mode target \
             --save_dir ${exp_record_dir} \
 	    --save_name M_${model_name}_D_${src}_${tar}_T_target \
-            --output_file $(pwd)/result/${src}_${tar}_bitgcf_result_${update_times}_target.txt \
-            --graph_file $(pwd)/graph/all_${src}_${tar}_${n_epoch}.graph \
+            --item_emb_path $(pwd)/graph/all_${src}_${tar}_${n_epoch}.graph \
+            --user_emb_path $(pwd)/graph/all_${src}_${tar}_${n_epoch}.graph \
             --src ${src} \
             --tar ${tar} \
             --model_name ${model_name} \
@@ -49,16 +56,19 @@ then
 	    --test_mode shared \
             --save_dir ${exp_record_dir} \
 	    --save_name M_${model_name}_D_${src}_${tar}_T_shared \
-            --output_file $(pwd)/result/${src}_${tar}_bitgcf_result_${update_times}_shared.txt \
-            --graph_file $(pwd)/graph/all_${src}_${tar}_${n_epoch}.graph \
+            --user_emb_path $(pwd)/graph/all_${src}_${tar}_${n_epoch}.graph \
+            --item_emb_path $(pwd)/graph/all_${src}_${tar}_${n_epoch}.graph \
             --src ${src} \
             --tar ${tar} \
             --model_name ${model_name} \
             --ncore ${ncore}
+    fi
 fi 
 
-if [[ $mode == "cold" ]]
+if [[ $normal_cold == "cold" ]]
 then
+    if [[ "$mode" == "train" || "$mode" == "traineval" ]]
+    then 
 	CUDA_VISIBLE_DEVICES=${gpu_id} \
         python3 ./BiTGCF/main.py\
             --data_path ${data_dir}/input_${ncore}core \
@@ -66,16 +76,21 @@ then
             --target_dataset cold_${tar} \
             --epoch ${n_epoch} \
             --batch_size 65536 \
-            --embed_size 25&&
+            --embed_size 25
+    fi
+
+    if [[ "$mode" == "eval" || "$mode" == "traineval" ]]
+    then
         python3 ../../rec_and_eval_ncore.py \
 	    --data_dir ${data_dir} \
 	    --test_mode cold \
             --save_dir ${exp_record_dir} \
 	    --save_name M_${model_name}_D_${src}_${tar}_T_cold \
-            --output_file $(pwd)/result/${src}_${tar}_bitgcf_result_${update_times}_cold.txt \
-            --graph_file $(pwd)/graph/all_${src}_cold_${tar}_${n_epoch}.graph \
+            --user_emb_path $(pwd)/graph/all_${src}_cold_${tar}_${n_epoch}.graph \
+            --item_emb_path $(pwd)/graph/all_${src}_cold_${tar}_${n_epoch}.graph \
             --src ${src} \
             --tar ${tar} \
             --model_name ${model_name}\
             --ncore ${ncore}
+    fi
 fi
