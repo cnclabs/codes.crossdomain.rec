d224d4c25072258eef3751d71129c5db21c11566
yth <yuting2727@gmail.com>
2022-12-31 08:32:45 +0000

    chore: dunno why lgn miss something from previous version
    --> delete all of them and add them back

--- models/LGN/data/__init__.py
+++
@@ -1,4 +0,0 @@
-from .sampler import PointwiseSampler
-from .sampler import PairwiseSampler
-from .sampler import TimeOrderPointwiseSampler
-from .sampler import TimeOrderPairwiseSampler
--- models/LGN/preprocess/run_preprocess.sh
+++
@@ -1,8 +0,0 @@
-declare -a datasets=("hk_csjj" "spo_csj" "mt_b")
-declare -A ncores
-mom_save_dir=$1
-ncores=(['hk_csjj']=5 ["spo_csj"]=5 ["mt_b"]=5)
-
-for d in "${datasets[@]}"; do
-	python3 generate_ncore_input.py --dataset ${d} --ncore ${ncores[$d]} --mom_save_dir ${mom_save_dir} --sample 3500
-done
