773a9385e14009a26363ef0e8d9b20d9c0f5be26
hyt <yuting2727@gmail.com>
2023-01-05 07:46:20 +0000

    refactor: rm not-used cpp code

--- models/CPR/src/optimizer/triplet_optimizer.h
+++ models/CPR/src/optimizer/triplet_optimizer.h
@@ -21,7 +21,6 @@ class TripletOptimizer {
 
         // functions
         double fast_sigmoid(double value);
-        double skew_opt(double prediction, double location, double scale);
 
         // loss
         int feed_margin_bpr_loss(std::vector<double>& from_embedding,
@@ -32,47 +31,5 @@ class TripletOptimizer {
                                  std::vector<double>& from_loss,
                                  std::vector<double>& to_loss_pos,
                                  std::vector<double>& to_loss_neg);
-        int feed_margin_bpr_loss_with_RotatE_transform(
-                                std::vector<double>& from_embedding, 
-                                std::vector<double>& relation_embedding, 
-                                std::vector<double>& to_embedding_pos, 
-                                std::vector<double>& to_embedding_neg, 
-                                double margin, 
-                                int dimension, 
-                                std::vector<double>& from_loss, 
-                                std::vector<double>& relation_loss, 
-                                std::vector<double>& to_loss_pos, 
-                                std::vector<double>& to_loss_neg);
-        void feed_bpr_loss(std::vector<double>& from_embedding,
-                           std::vector<double>& to_embedding_pos,
-                           std::vector<double>& to_embedding_neg,
-                           int dimension,
-                           std::vector<double>& from_loss,
-                           std::vector<double>& to_loss);
-        int feed_hoprec_loss(std::vector<double>& from_embedding,
-                             std::vector<double>& to_embedding_pos,
-                             std::vector<double>& to_embedding_neg,
-                             double margin,
-                             int dimension,
-                             std::vector<double>& from_loss,
-                             std::vector<double>& to_loss);
-        void feed_trans_loss(std::vector<double>& from_embedding,
-                             std::vector<double>& relation_embedding,
-                             std::vector<double>& to_embedding,
-                             double label,
-                             int dimension,
-                             std::vector<double>& from_loss,
-                             std::vector<double>& relation_loss,
-                             std::vector<double>& to_loss);
-        int feed_skew_opt_loss(std::vector<double>& from_embedding,
-                             std::vector<double>& to_embedding_pos,
-                             std::vector<double>& to_embedding_neg,
-                             double location,
-                             double scale,
-                             int dimension,
-                             std::vector<double>& from_loss,
-                             std::vector<double>& to_loss);
-
-
 };
 #endif
