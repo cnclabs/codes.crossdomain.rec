38cd248b0e5eb1b2de790daa4e99f776afd2b481
hyt <yuting2727@gmail.com>
2023-01-07 14:04:20 +0000

    chore: proceed if nothing wrong

--- preprocess/run_pre_sample_all.sh
+++ preprocess/run_pre_sample_all.sh
@@ -1,18 +1,18 @@
 data_dir=$1
-src=hk
-tar=csjj
+src=hk&&
+tar=csjj&&
 bash run_pre_sample_single.sh ${src} ${tar} target ${data_dir}&&
 bash run_pre_sample_single.sh ${src} ${tar} shared ${data_dir}&&
 bash run_pre_sample_single.sh ${src} ${tar} cold ${data_dir}&&
 
-src=mt
-tar=b
+src=mt&&
+tar=b&&
 bash run_pre_sample_single.sh ${src} ${tar} target ${data_dir}&&
 bash run_pre_sample_single.sh ${src} ${tar} shared ${data_dir}&&
 bash run_pre_sample_single.sh ${src} ${tar} cold ${data_dir}&&
 
-src=spo
-tar=csj
+src=spo&&
+tar=csj&&
 bash run_pre_sample_single.sh ${src} ${tar} target ${data_dir}&&
 bash run_pre_sample_single.sh ${src} ${tar} shared ${data_dir}&&
 bash run_pre_sample_single.sh ${src} ${tar} cold ${data_dir}
