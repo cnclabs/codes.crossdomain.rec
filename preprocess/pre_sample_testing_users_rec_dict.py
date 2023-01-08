03f3d3bf67ab440284a407b238e05bdde021795b
hyt <yuting2727@gmail.com>
2023-01-08 01:51:15 +0000

    [TODO]: check if ignore cold-start items
    currently, neg99_pos1 might only contains neg 99.
    since previous people filter out cold-start items when saving out
    tar_test df.

--- preprocess/pre_sample_testing_users_rec_dict.py
+++ preprocess/pre_sample_testing_users_rec_dict.py
@@ -25,6 +25,8 @@ def get_testing_users_rec_dict(n_worker, tar_test_df, uid_u, uid_i, test_mode, d
     testing_users_rec_dict = {}
     for r in results:
         testing_users_rec_dict.update(r)
+    print('Total users:', len(testing_users))
+    print('Remain user:', len(testing_users_rec_dict))
     print("Done generating testing users' positive-negative pairs.")
 
     return testing_users_rec_dict
@@ -37,14 +39,31 @@ def get_testing_users(test_mode, data_input_dir, src, tar):
     return testing_users
 
 def process_user_pos_neg_pair(tar_train_df, tar_test_df, uid_u, uid_i, total_item_set,  user_list):
+    #TODO (katieyth): check if ignore cold-start items
+
+    #warm_start_items = list(tar_train_df[uid_i].unique())
+    #n_cold_start_item = 0
     user_rec_dict = {}
     for user in user_list:
         pos_pool = set(tar_train_df[tar_train_df[uid_u] == user][uid_i])
         neg_pool = total_item_set - pos_pool
         neg_99 = random.sample(neg_pool, 99)
-    
+        
+#        the_one_out = list(tar_test_df[tar_test_df[uid_u] == user][uid_i])[0]
+        
+#        # we don't consider the cold-start item scenario
+#        if the_one_out not in warm_start_items:
+#            n_cold_start_item +=1
+#            #print("****cold start item:", the_one_out)
+#        else:
+#            user_rec_pool = list(neg_99) + list(tar_test_df[tar_test_df[uid_u] == user][uid_i])
+#            assert len(user_rec_pool) == 100, f'length should be 100 but get {len(user_rec_pool)}'
+#            user_rec_dict[user] = user_rec_pool
+
         user_rec_pool = list(neg_99) + list(tar_test_df[tar_test_df[uid_u] == user][uid_i])
         user_rec_dict[user] = user_rec_pool
+
+    #print(f'Ignore due to cold-start item:', n_cold_start_item)
     
     return user_rec_dict
 
