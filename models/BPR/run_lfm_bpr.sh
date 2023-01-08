52a8d5a83a06b9c1766f026e80a6bd0bc5f666b1
yth <yuting2727@gmail.com>
2023-01-08 10:13:20 +0000

    chore: put params to global args

--- models/BPR/run_lfm_bpr.sh
+++ models/BPR/run_lfm_bpr.sh
@@ -2,82 +2,76 @@
 ncore_data_dir=$1
 cpr_input_dir=$2
 bpr_input_dir=$3
-model_save_dir=$4
+emb_save_dir=$4
 exp_record_dir=$5
 mode=$6
+src=$7
+tar=$8
 model_name=bpr
 update_times=200
 dim=100
-declare -a datasets=("hk_csjj" "spo_csj" "mt_b")
-declare -A ncores
-ncores=(['hk_csjj']=5 ["spo_csj"]=5 ["mt_b"]=5)
 sample=3500
+worker=20
+l2_reg=0.00001
 
-if [[ ! -d ${model_save_dir} ]]
+if [[ ! -d ${emb_save_dir} ]]
 then
-        mkdir -p ${model_save_dir}
+        mkdir -p ${emb_save_dir}
 fi
 
 for d in "${datasets[@]}"; do
-    IFS='_'
-    read -a domains <<< "$d"
-    IFS=' '
-    src=${domains[0]}
-    tar=${domains[1]}
-    ncore=${ncores[$d]}
-
     if [[ "$mode" == "train" || "$mode" == "traineval" ]]
     then 
     # all input (target)
     python3 ./lfm_bpr.py \
     --train ${bpr_input_dir}/${src}_${tar}_src_tar_train_input.txt \
-    --save ${model_save_dir}/${src}_${tar}_src_tar.txt \
-    --dim 100 \
-    --iter $update_times \
-    --worker 20 \
-    --item_alpha 0.00001 \
-    --user_alpha 0.00001 || exit 1
+    --save ${emb_save_dir}/${src}_${tar}_src_tar.txt \
+    --dim ${dim} \
+    --iter ${update_times} \
+    --worker ${worker} \
+    --item_alpha ${l2_reg} \
+    --user_alpha ${l2_reg} || exit 1
 
     # all input (cold)
 
     python3 ./lfm_bpr.py \
     --train ${bpr_input_dir}/${src}_${tar}_src_ctar_train_input.txt \
-    --save ${model_save_dir}/${src}_${tar}_src_ctar.txt \
-    --dim 100 \
-    --iter $update_times \
-    --worker 20 \
-    --item_alpha 0.00001 \
-    --user_alpha 0.00001 || exit 1
+    --save ${emb_save_dir}/${src}_${tar}_src_ctar.txt \
+    --dim ${dim} \
+    --iter ${update_times} \
+    --worker ${worker} \
+    --item_alpha ${l2_reg} \
+    --user_alpha ${l2_reg} || exit 1
 
     ### tar
     python3 ./lfm_bpr.py \
     --train ${cpr_input_dir}/${tar}_tar_train_input.txt \
-    --save ${model_save_dir}/${tar}_tar.txt \
-    --dim 100 \
-    --iter $update_times \
-    --worker 20 \
-    --item_alpha 0.00001 \
-    --user_alpha 0.00001 || exit 1
+    --save ${emb_save_dir}/${tar}_tar.txt \
+    --dim ${dim} \
+    --iter ${update_times} \
+    --worker ${worker} \
+    --item_alpha ${l2_reg} \
+    --user_alpha ${l2_reg} || exit 1
 
     ### src
     python3 ./lfm_bpr.py \
     --train ${cpr_input_dir}/${src}_src_train_input.txt \
-    --save ${model_save_dir}/${src}_tar.txt \
-    --dim 100 \
-    --iter $update_times \
-    --worker 20 \
-    --item_alpha 0.00001 \
-    --user_alpha 0.00001  || exit 1
+    --save ${emb_save_dir}/${src}_tar.txt \
+    --dim ${dim} \
+    --iter ${update_times} \
+    --worker ${worker} \
+    --item_alpha ${l2_reg} \
+    --user_alpha ${l2_reg}  || exit 1
 
     ### tar cold
     python3 ./lfm_bpr.py \
     --train ${cpr_input_dir}/${tar}_ctar_train_input.txt \
-    --save ${model_save_dir}/${tar}_ctar.txt \
-    --dim 100 \
-    --iter $update_times \
-    --worker 20 \
-    --item_alpha 0.00001 \
-    --user_alpha 0.00001 || exit 1
+    --save ${emb_save_dir}/${tar}_ctar.txt \
+    --dim ${dim} \
+    --iter ${update_times} \
+    --worker ${worker} \
+    --item_alpha ${l2_reg} \
+    --user_alpha ${l2_reg} || exit 1
 
 
 
@@ -91,24 +85,22 @@ for d in "${datasets[@]}"; do
 	--test_mode target \
         --save_dir ${exp_record_dir} \
 	--save_name M_${model_name}_D_${src}_${tar}_T_target \
-	--user_emb_path ${model_save_dir}/${tar}_tar.txt \
-	--item_emb_path ${model_save_dir}/${tar}_tar.txt \
+	--user_emb_path ${emb_save_dir}/${tar}_tar.txt \
+	--item_emb_path ${emb_save_dir}/${tar}_tar.txt \
 	--src ${src} \
 	--tar ${tar} \
-        --model_name ${model_name} \
-	--ncore ${ncore} || exit 1
+        --model_name ${model_name} || exit 1
 
     python3 ../../rec_and_eval_ncore.py \
 	--ncore_data_dir ${ncore_data_dir} \
 	--test_mode shared \
         --save_dir ${exp_record_dir} \
 	--save_name M_${model_name}_D_${src}_${tar}_T_shared \
-	--user_emb_path ${model_save_dir}/${tar}_tar.txt \
-	--item_emb_path ${model_save_dir}/${tar}_tar.txt \
+	--user_emb_path ${emb_save_dir}/${tar}_tar.txt \
+	--item_emb_path ${emb_save_dir}/${tar}_tar.txt \
 	--src ${src} \
 	--tar ${tar} \
-        --model_name ${model_name} \
-	--ncore ${ncore} || exit 1
+        --model_name ${model_name} || exit 1
 
     ### eval @tar @shared
     python3 ../../rec_and_eval_ncore.py \
@@ -116,24 +108,22 @@ for d in "${datasets[@]}"; do
 	--test_mode target \
         --save_dir ${exp_record_dir} \
 	--save_name M_${model_name}_s_D_${src}_${tar}_T_target \
-	--user_emb_path ${model_save_dir}/${src}_${tar}_src_tar.txt \
-	--item_emb_path ${model_save_dir}/${src}_${tar}_src_tar.txt \
+	--user_emb_path ${emb_save_dir}/${src}_${tar}_src_tar.txt \
+	--item_emb_path ${emb_save_dir}/${src}_${tar}_src_tar.txt \
 	--src ${src} \
 	--tar ${tar} \
-        --model_name ${model_name}_s \
-	--ncore ${ncore} || exit 1
+        --model_name ${model_name}_s || exit 1
 
     python3 ../../rec_and_eval_ncore.py \
 	--ncore_data_dir ${ncore_data_dir} \
 	--test_mode shared \
         --save_dir ${exp_record_dir} \
 	--save_name M_${model_name}_s_D_${src}_${tar}_T_shared \
-	--user_emb_path ${model_save_dir}/${src}_${tar}_src_tar.txt \
-	--item_emb_path ${model_save_dir}/${src}_${tar}_src_tar.txt \
+	--user_emb_path ${emb_save_dir}/${src}_${tar}_src_tar.txt \
+	--item_emb_path ${emb_save_dir}/${src}_${tar}_src_tar.txt \
 	--src ${src} \
 	--tar ${tar} \
-        --model_name ${model_name}_s\
-	--ncore ${ncore} || exit 1
+        --model_name ${model_name}_s || exit 1
 
     ### eval @cold
     python3 ../../rec_and_eval_ncore.py \
@@ -141,11 +131,10 @@ for d in "${datasets[@]}"; do
 	--test_mode cold \
         --save_dir ${exp_record_dir} \
 	--save_name M_${model_name}_s_D_${src}_${tar}_T_cold \
-	--user_emb_path ${model_save_dir}/${src}_${tar}_src_ctar.txt \
-	--item_emb_path ${model_save_dir}/${src}_${tar}_src_ctar.txt \
+	--user_emb_path ${emb_save_dir}/${src}_${tar}_src_ctar.txt \
+	--item_emb_path ${emb_save_dir}/${src}_${tar}_src_ctar.txt \
 	--src ${src} \
 	--tar ${tar} \
-        --model_name ${model_name}_s\
-	--ncore ${ncore} || exit 1
+        --model_name ${model_name}_s || exit 1
     fi
 done
