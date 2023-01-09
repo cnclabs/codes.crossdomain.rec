83659648c792e6c41fe0c4dbe70a1b3ec6a7f1da
hyt <yuting2727@gmail.com>
2023-01-09 11:58:18 +0000

    chore: rm not-used

--- preprocess/generate_bpr_input.sh
+++ preprocess/generate_bpr_input.sh
@@ -2,8 +2,6 @@ cpr_input_dir=$1
 bpr_input_dir=$2
 
 declare -a datasets=("hk_csjj" "spo_csj" "mt_b")
-declare -A ncores
-ncores=(["hk_csjj"]=5 ["spo_csj"]=5 ["mt_b"]=5)
 
 for d in "${datasets[@]}";
 do
@@ -12,7 +10,6 @@ do
     IFS=" "
     src=${domains[0]}
     tar=${domains[1]}
-    ncore=${ncores[$d]}
 
     python3 tools/generate_bpr_input.py \
         --cpr_input_dir ${cpr_input_dir} \
