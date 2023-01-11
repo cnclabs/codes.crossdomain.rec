2ff3f19875777fad06e2e6ff6912298b738eb32c
yth <yuting2727@gmail.com>
2023-01-11 09:44:05 +0000

    chore: as above

--- models/EMCDR/run_train.sh
+++ models/EMCDR/run_train.sh
@@ -11,18 +11,15 @@ for d in "${datasets[@]}"; do
     IFS=' '
     src=${domains[0]}
     tar=${domains[1]}
-    ncore=${ncores[$d]}
     
     # normal
     python3 MLP.py \
-    --epoch_log ./${d}/model_log/MLP_lightfm_tv_vod.txt \
     --model_save_dir ${emcdr_emb_dir}/${src}_${tar}_src_tar \
     --Us ${emcdr_input_dir}/${src}_${tar}_src_tar_Us.pickle \
     --Ut ${emcdr_input_dir}/${src}_${tar}_src_tar_Ut.pickle || exit 1;
     
     # cold
     python3 MLP.py \
-    --epoch_log ./${d}/model_log_cold/MLP_lightfm_tv_vod_cold.txt \
     --model_save_dir ${emcdr_emb_dir}/${src}_${tar}_src_ctar \
     --Us ${emcdr_input_dir}/${src}_${tar}_src_ctar_Us.pickle \
     --Ut ${emcdr_input_dir}/${src}_${tar}_src_ctar_Ut.pickle || exit 1;
@@ -30,7 +27,6 @@ done
 
 update_times=1000 
 num_checkpoint=1
-split=$((200/$num_checkpoint))
 
 for d in "${datasets[@]}"; do
     IFS='_'
@@ -38,24 +34,21 @@ for d in "${datasets[@]}"; do
 	IFS=' '
 	src=${domains[0]}
 	tar=${domains[1]}
-	ncore=${ncores[$d]}
 
     for checkpoint in $(seq 1 $num_checkpoint); do
-        current_split=$(($checkpoint*$split))
-
         python3 infer_Us.py \
 	    --user_to_infer_path ${ncore_data_dir}/${src}_${tar}_src_tar_sample_testing_shared_users.pickle \
 	    --Us_path ${emcdr_input_dir}/${src}_${tar}_src_tar_Us.pickle \
-            --meta_path ${emcdr_emb_dir}/${src}_${tar}_src_tar/mlp_epoch_${update_times}/mlp_epoch_${update_times}.meta \
-            --ckpt_path ${emcdr_emb_dir}/${src}_${tar}_src_tar/mlp_epoch_${update_times} \
+            --meta_path ${emcdr_emb_dir}/${src}_${tar}_src_tar/mlp.meta \
+            --ckpt_path ${emcdr_emb_dir}/${src}_${tar}_src_tar/ \
             --emb_save_path ${emcdr_emb_dir}/${src}_${tar}_src_tar_shared.pickle || exit 1;
 
         # cold
         python3 infer_Us.py \
 	    --user_to_infer_path ${ncore_data_dir}/${src}_${tar}_src_tar_sample_testing_cold_users.pickle \
 	    --Us_path ${emcdr_input_dir}/${src}_${tar}_src_ctar_cold_Us.pickle \
-            --meta_path ${emcdr_emb_dir}/${src}_${tar}_src_ctar/mlp_epoch_${update_times}/mlp_epoch_${update_times}.meta \
-            --ckpt_path ${emcdr_emb_dir}/${src}_${tar}_src_ctar/mlp_epoch_${update_times} \
+            --meta_path ${emcdr_emb_dir}/${src}_${tar}_src_ctar/mlp.meta \
+            --ckpt_path ${emcdr_emb_dir}/${src}_${tar}_src_ctar/ \
             --emb_save_path ${emcdr_emb_dir}/${src}_${tar}_src_ctar_cold.pickle || exit 1;
     done
 done
