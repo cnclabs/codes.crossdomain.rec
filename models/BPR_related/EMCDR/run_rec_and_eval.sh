b42981f21da62aa23129116500e113304c2a051d
yth <yuting2727@gmail.com>
2023-01-11 08:33:45 +0000

    chore: fit to the aforementioned code cleaning

--- models/BPR_related/EMCDR/run_rec_and_eval.sh
+++ models/BPR_related/EMCDR/run_rec_and_eval.sh
@@ -1,66 +1,51 @@
 #!/bin/bash
-set -xe
-
-data_dir=$1
+ncore_data_dir=$1
 exp_record_dir=$2
+emcdr_emb_dir=$3
+bpr_emb_dir=$4
 model_name=emcdr
-update_times=200 
-method="EMCDR"
 
 declare -a datasets=("hk_csjj" "spo_csj" "mt_b")
-declare -A ncores
-ncores=(['hk_csjj']=5 ["spo_csj"]=5 ["mt_b"]=5)
-
-echo pwd
 
 for d in "${datasets[@]}"; do
-	IFS='_'
-	read -a domains <<< "$d"
-	IFS=' '
-	src=${domains[0]}
-	tar=${domains[1]}
-	ncore=${ncores[$d]}
+    IFS='_'
+    read -a domains <<< "$d"
+    IFS=' '
+    src=${domains[0]}
+    tar=${domains[1]}
+
+    #python3 ../../../rec_and_eval_ncore.py \
+    #--ncore_data_dir ${ncore_data_dir} \
+    #--test_mode target \
+    #--save_dir ${exp_record_dir} \
+    #--save_name M_${model_name}_D_${src}_${tar}_T_target \
+    #--user_emb_path_shared ${emcdr_emb_dir}/${src}_${tar}_shared.pickle\
+    #--user_emb_path_target ${bpr_emb_dir}/${tar}_tar.txt \
+    #--item_emb_path ${bpr_emb_dir}/${tar}_tar.txt \
+    #--src ${src} \
+    #--tar ${tar} \
+    #--model_name ${model_name} || exit 1
+
+    #python3 ../../../rec_and_eval_ncore.py \
+    #--ncore_data_dir ${ncore_data_dir} \
+    #--test_mode shared \
+    #--save_dir ${exp_record_dir} \
+    #--save_name M_${model_name}_D_${src}_${tar}_T_shared \
+    #--user_emb_path_shared ${emcdr_emb_dir}/${src}_${tar}_shared.pickle\
+    #--user_emb_path_target ${bpr_emb_dir}/${tar}_tar.txt\
+    #--item_emb_path ${bpr_emb_dir}/${tar}_tar.txt \
+    #--src ${src} \
+    #--tar ${tar} \
+    #--model_name ${model_name} || exit 1
 
     python3 ../../../rec_and_eval_ncore.py \
-    --data_dir ${data_dir} \
-    --test_mode target \
-    --save_dir ${exp_record_dir} \
-    --save_name M_${model_name}_D_${src}_${tar}_T_target \
-    --src ${src} \
-    --tar ${tar} \
-    --n_worker 8 \
-    --ncore $ncore \
-    --model_name ${model_name} \
-    --item_emb_path $(pwd)/../lfm_bpr_graphs/${tar}_lightfm_bpr_${update_times}_10e-5.txt \
-    --user_emb_path_shared $(pwd)/${src}_${tar}/shared_users_mapped_emb_dict_${update_times}.pickle\
-    --user_emb_path_target $(pwd)/../lfm_bpr_graphs/${tar}_lightfm_bpr_${update_times}_10e-5.txt
-
-
-    python3 ../../../rec_and_eval_ncore.py \
-    --data_dir ${data_dir} \
-    --test_mode shared \
-    --save_dir ${exp_record_dir} \
-    --save_name M_${model_name}_D_${src}_${tar}_T_shared \
-    --src ${src} \
-    --tar ${tar} \
-    --n_worker 8 \
-    --ncore $ncore\
-    --model_name ${model_name} \
-    --item_emb_path $(pwd)/../lfm_bpr_graphs/${tar}_lightfm_bpr_${update_times}_10e-5.txt \
-    --user_emb_path_shared $(pwd)/${src}_${tar}/shared_users_mapped_emb_dict_${update_times}.pickle\
-    --user_emb_path_target $(pwd)/../lfm_bpr_graphs/${tar}_lightfm_bpr_${update_times}_10e-5.txt
-
-
-    python3 ../../../rec_and_eval_ncore.py \
-    --data_dir ${data_dir} \
+    --ncore_data_dir ${ncore_data_dir} \
     --test_mode cold \
     --save_dir ${exp_record_dir} \
     --save_name M_${model_name}_D_${src}_${tar}_T_cold \
+    --user_emb_path_cold ${emcdr_emb_dir}/${src}_${tar}_cold.pickle\
+    --item_emb_path ${bpr_emb_dir}/${tar}_ctar.txt \
     --src ${src} \
     --tar ${tar} \
-    --n_worker 8 \
-    --ncore $ncore \
-    --model_name emcdr\
-    --item_emb_path $(pwd)/../lfm_bpr_graphs/cold_${tar}_lightfm_bpr_${update_times}_10e-5.txt \
-    --user_emb_path_cold $(pwd)/${src}_${tar}/cold_users_mapped_emb_dict_${update_times}.pickle
+    --model_name ${model_name} || exit 1
 done
