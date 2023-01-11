d9d3ce498b9d911288700bf94e4867a83b7b0a33
yth <yuting2727@gmail.com>
2023-01-11 08:33:12 +0000

    refactor: use the refactored script

--- models/BPR_related/EMCDR/run_preprocess.sh
+++ models/BPR_related/EMCDR/run_preprocess.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
-set -xe
-
-mom_save_dir=$1
-update_times=200 
-num_checkpoint=1
-split=$(($update_times/$num_checkpoint))
+ncore_data_dir=$1
+bpr_emb_dir=$2
+emcdr_input_dir=$3
 
 declare -a datasets=("hk_csjj" "spo_csj" "mt_b")
-declare -A ncores
-ncores=(['hk_csjj']=5 ["spo_csj"]=5 ["mt_b"]=5)
 
-for d in "${datasets[@]}"; do
-    mkdir $d
+for d in "${datasets[@]}"; 
+do
+    IFS='_'
+    read -a domains <<< "$d"
+    IFS=' '
+    src=${domains[0]}
+    tar=${domains[1]}
 
-	IFS='_'
-	read -a domains <<< "$d"
-	IFS=' '
-	src=${domains[0]}
-	tar=${domains[1]}
-	ncore=${ncores[$d]}
+    # normal 
+    python3 preprocess.py \
+    --share_user_path ${ncore_data_dir}/${src}_${tar}_src_tar_all_shared_users.pickle \
+    --pretrained_source_emb_path ${bpr_emb_dir}/${src}_src.txt\
+    --pretrained_target_emb_path ${bpr_emb_dir}/${tar}_tar.txt\
+    --Us_save_path ${emcdr_input_dir}/${src}_${tar}_src_tar_Us.pickle\
+    --Vs_save_path ${emcdr_input_dir}/${src}_${tar}_src_tar_Vs.pickle\
+    --Ut_save_path ${emcdr_input_dir}/${src}_${tar}_src_tar_Ut.pickle\
+    --Vt_save_path ${emcdr_input_dir}/${src}_${tar}_src_tar_Vt.pickle\
+    --target_item_list_save_path ${emcdr_input_dir}/${src}_${tar}_src_tar_target_item_list.pickle || exit 1;\
 
-    for checkpoint in $(seq 1 $num_checkpoint); do
-        current_split=$(($checkpoint*$split))
-        
-        python3 preprocess.py \
-	--mom_save_dir ${mom_save_dir} \
-        --dataset_name $d \
-        --current_epoch $current_split \
-        --ncore $ncore
+    # cold
+    python3 preprocess.py \
+    --share_user_path ${ncore_data_dir}/${src}_${tar}_src_tar_all_shared_users.pickle \
+    --cold_user_path ${ncore_data_dir}/${src}_${tar}_src_tar_sample_testing_cold_users.pickle \
+    --pretrained_source_emb_path ${bpr_emb_dir}/${src}_src.txt\
+    --pretrained_target_emb_path ${bpr_emb_dir}/${tar}_ctar.txt\
+    --Us_save_path ${emcdr_input_dir}/${src}_${tar}_src_ctar_Us.pickle\
+    --cold_Us_save_path ${emcdr_input_dir}/${src}_${tar}_src_ctar_cold_Us.pickle\
+    --Vs_save_path ${emcdr_input_dir}/${src}_${tar}_src_ctar_Vs.pickle\
+    --Ut_save_path ${emcdr_input_dir}/${src}_${tar}_src_ctar_Ut.pickle\
+    --Vt_save_path ${emcdr_input_dir}/${src}_${tar}_src_ctar_Vt.pickle\
+    --target_item_list_save_path ${emcdr_input_dir}/${src}_${tar}_src_ctar_target_item_list.pickle || exit 1;\
 
-        python3 preprocess_cold.py \
-	--mom_save_dir ${mom_save_dir} \
-        --current_epoch $current_split \
-        --dataset_name $d \
-        --ncore $ncore
-    done
 done
