3fc030e7dc11491be670211204c6f5c8da7d48c3
yth <yuting2727@gmail.com>
2023-04-02 12:32:39 +0000

    chore: remove unnecessary parts

--- models/Bi-TGCF/eval.sh
+++
@@ -1,9 +0,0 @@
-python3 ./rec_and_eval_csj_hk.py \
---test_users target \
---output_file ../Bi-TGCF_result/all_csj_hk_target_result.txt \
---graph_file ../Bi-TGCF_result/all_csj_hk.graph
-
-python3 ./rec_and_eval_csj_hk.py \
---test_users share \
---output_file ../Bi-TGCF_result/all_csj_hk_share_result.txt \
---graph_file ../Bi-TGCF_result/all_csj_hk.graph
--- models/Bi-TGCF/test.sh
+++
@@ -1,1 +0,0 @@
-screen -dm -S test bash -c "echo 123; sleep 2; echo 321; exec sh"
